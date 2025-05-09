43dc6a33709b6d0d63d022965400b14dfbb4ced6
people9953 <people99532@gmail.com>
2025-05-09 10:40:28 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const buttons = document.querySelectorAll('.buttons button'); // 모든 버튼 �
 buttons.forEach(button => { // 각 버튼에 클릭 이벤트 리스너를 추가
     button.addEventListener('click', () => { //버튼을 클릭하면 {}에있는 코드 실행
         const buttonValue = button.textContent; // 클릭된 버튼의 값을 가져오기
+        console.log(`${buttonValue}`);
         const currentDisplay = resultDisplay.textContent; // 현재 화면에 표시된 내용을 가져온다
         if (button.classList.contains('number')) {  // 만약 클릭된 버튼이 number 클래스를 가질때 (숫자 또는 소수점)
             if (buttonValue === '.') { // 클릭한 버튼이 소수점('.') 버튼일때
@@ -22,7 +23,6 @@ buttons.forEach(button => { // 각 버튼에 클릭 이벤트 리스너를 추�
             if (buttonValue === 'C') {  // 클릭한 버튼의 값이 'C' 일때
                 resultDisplay.textContent = '0'; // 화면에 표시된 값을 '0'으로 바꿔서 초기화하기
             }
-            ㅇ
             // ±, % 같은 다른 function 버튼 기능 추가 자리
 
         } else if (button.classList.contains('operator')) { // 클릭한 버튼이 operator 클래스일때
